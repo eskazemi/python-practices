61112c8003bbd5fdb6e80a4016ead6267019a711
Esmaeil kazemi <smk182018@gmail.com>
2022-10-31 15:29:15 +0330

    Feat(archive): archive JSON file
    -Saving the list of prices per request as a json file
    - create archive folder

--- exchange/main.py
+++ exchange/main.py
@@ -1,5 +1,6 @@
 import requests
 from config import URL, HEADERS
+import json
 
 payload = {}
 
@@ -7,10 +8,19 @@ payload = {}
 def get_rate():
     response = requests.request("GET", URL, headers=HEADERS, data=payload)
 
-    status_code = response.status_code
-    result = response.text
-    return result
+    if response.status_code == 200:
+        return json.loads(response.text)
+    else:
+        return None
+
+
+def archive(filename, rates):
+
+    with open(f'archive/{filename}.json', 'w') as f:
+        f.write(json.dumps(rates))
+
 
 
 if __name__ == "__main__":
-    print(get_rate())
+    res = get_rate()
+    archive(res["timestamp"], res["rates"])
