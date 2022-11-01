ee0ac8ef17296ab4deda47a5d383250a8e1efaa0
Esmaeil kazemi <smk182018@gmail.com>
2022-11-01 10:52:57 +0330

    Feat(emaeil): config send emaeil
    -set rules in config
    -create mail for send emaeil
    -create send_emaeil function

--- exchange/config.py
+++ exchange/config.py
@@ -4,3 +4,11 @@ URL = \
 HEADERS = {
     "apikey": "YmdgCmACaGFPQcwTU8KhMtaMuz5X4ZrN"
 }
+
+
+rules = {
+    "archive": True,
+    "send_email": True,
+    # preferred default is None
+    "preferred": ["BTC", "IRR", "USD", "CAD", "IQD"]
+}
--- exchange/main.py
+++ exchange/main.py
@@ -1,5 +1,5 @@
 import requests
-from config import URL, HEADERS
+from config import URL, HEADERS, rules
 import json
 
 payload = {}
@@ -15,12 +15,24 @@ def get_rate():
 
 
 def archive(filename, rates):
-
     with open(f'archive/{filename}.json', 'w') as f:
         f.write(json.dumps(rates))
 
 
+def send_email(date, rates):
+    subject = f"{date} rates"
+    if rules["preferred"] is not None:
+        tmp = dict()
+        for exc in rules["preferred"]:
+            tmp = dict()
+            tmp[exc] = rates[exc]
+        rates = tmp
+    body = json.dumps(rates)
+
 
 if __name__ == "__main__":
     res = get_rate()
-    archive(res["timestamp"], res["rates"])
+    if rules["archive"]:
+        archive(res["timestamp"], res["rates"])
+    if rules["send_email"]:
+        send_email(res["timestamp"], res["rates"])
