ee5ad70a080290980e3b5d11664d19f5ec38fbc6
Esmaeil kazemi <smk182018@gmail.com>
2022-12-04 12:00:56 +0330

    create(): create search insert algorithm

---
+++ algorithms/search/search_insert.py
@@ -0,0 +1,32 @@
+def search_insert(array,val):
+    """
+    Given a sorted array and a target value, return the index if the target is
+    found. If not, return the index where it would be if it were inserted in order.
+    For example:
+    [1,3,5,7], 5 -> 2
+    [1,3,5,9], 2 -> 1
+    [1,3,5,6], 7 -> 4
+    [1,3,5,6], 0 -> 0
+    
+    """
+    low=0
+    high=len(array)-1
+    mid=high//2
+
+    while low <= high:
+        if val > array[mid]:
+            mid +=1
+            low=mid
+        else:
+            mid -=1
+            high=mid
+    return low
+
+
+def main():
+    array = list(map(int, input("array: ").split(" ")))
+    value = int(input("value: "))
+    print(search_insert(array, value))
+
+
+main()
